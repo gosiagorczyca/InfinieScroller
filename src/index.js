98b5e8eaee473aefb6e90afe62b98ff3b7ebca88
Małgorzata Gorczyca <gosia.gorczyca@gmail.com>
2022-10-12 13:23:13 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
